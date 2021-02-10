17c6b4f3fa4cfe42531cb95186572e72fdad25cd
monadgon <monad.gon@gmail.com>
2021-02-10 15:19:02 +0900

    cosmos

--- recursion.py
+++ recursion.py
@@ -1,13 +1,26 @@
 def fib(x):
-    if x == 0 or x == 1:
-        return 1
+    if x < 2:
+        return x
     else:
         return fib(x-1) + fib(x-2)
 
-print(fib(4)) # result is 5
+print("result:", fib(6))
 
 ## 위의 로직을 한 줄로 표현하면 다음과 같습니다.
-def fibonacciSeqquence(x):
-    return fibonacciSeqquence(x-1) + fibonacciSeqquence(x-2) if x >= 2 else 1 
+#def fibonacciSeqquence(x):
+#    return fibonacciSeqquence(x-1) + fibonacciSeqquence(x-2) if x >= 2 else x 
 
-print(fibonacciSeqquence(4))
+#print(fibonacciSeqquence(4))
+
+# Fibonacci Problem
+# Sample Input: 6  -> Sample Output: 0 1 1 2 3 5
+def fibPrint(x):
+    rtn = 0
+    if x < 2:
+        rtn =  x
+    else:
+        rtn = fibPrint(x-1) + fibPrint(x-2)
+
+    return rtn
+
+print("result:", fibPrint(6))
